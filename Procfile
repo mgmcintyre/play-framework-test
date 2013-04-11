e6cd609ab890702ef1f28eb56c7e8e9920a5f25d
Mark McIntyre <mmcintyre@groupcommerce.com>
2013-04-11 13:28:35 -0400

    Added Redis caching for 1 day to race

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: target/start -Dhttp.port=${PORT} -DapplyEvolutions.default=true -Ddb.default.url=${DATABASE_URL} -Ddb.default.driver=org.postgresql.Driver
+web: target/start -Dhttp.port=${PORT} -DapplyEvolutions.default=true 
+  -Dredis.host=squawfish.redistogo.com -Dredis.port=9263
+  -Dredis.password=5373308a3f1e82c6b297e06e0f408ebd
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -20,56 +20,61 @@ object Application extends Controller {
   }
 
   def redirectMe = Action {
-    val firstChoice: List[String] = List(
-      "https://about.me/mgmcintyre",
-      "about-me")
-    val secondChoice: List[String] = List(
-      "http://linkedin.com/in/mgmcintyre",
-      "linkedin")
-    Async {
-      val start = System.currentTimeMillis()
-      def getLatency(r: Any): Long = System.currentTimeMillis() - start
 
-      val firstTime = WS.url(firstChoice(0)).get().map(getLatency)
-      val secondTime = WS.url(secondChoice(0)).get().map(getLatency)
+    play.api.cache.Cache.getAs[JsObject]("race.results") match {
+      case json: Some[JsObject] => {
+        Logger.debug("Results available in the cache")
+        Ok(json.get)
+      }
+      case _ => {
+        Logger.debug("Results not available in the cache")
+        val firstChoice: List[String] = List(
+          "https://about.me/mgmcintyre",
+          "about-me")
+        val secondChoice: List[String] = List(
+          "http://linkedin.com/in/mgmcintyre",
+          "linkedin")
+        Async {
+          val start = System.currentTimeMillis()
+          def getLatency(r: Any): Long = System.currentTimeMillis() - start
 
-      Future.sequence(Seq(firstTime, secondTime)).map { times =>
-          play.api.Logger.debug("First: " + times(0) + ", Second: " + times(1))
-          
-          val choice = if (times(0) > times(1)) secondChoice
-          else firstChoice
+          val firstTime = WS.url(firstChoice(0)).get().map(getLatency)
+          val secondTime = WS.url(secondChoice(0)).get().map(getLatency)
 
-          play.api.Logger.debug("Choice: " + choice(0))
-          val json = Json.obj(
-            "status" -> "OK",
-            "redirect" -> Json.obj (
-              "winner" -> choice(0),
-              "results" -> Json.arr (
-                Json.obj (
-                  "url" -> firstChoice(0),
-                  "name" -> firstChoice(1),
-                  "time" -> times(0)
-                ),
-                Json.obj (
-                  "url" -> secondChoice(0),
-                  "name" -> secondChoice(1),
-                  "time" -> times(1)
-                )
-              )
-            )
-          )
-          Ok(json)
-      } recover {
-        case e =>
-          play.api.Logger.warn("Issue fetching url: " + e.getMessage)
-          val json = Json.obj(
-            "status" -> "KO",
-            "errors" -> Json.arr(
-              "Something went wrong!",
-              e.getMessage
-            )
-          )
-          InternalServerError(json)
+          Future.sequence(Seq(firstTime, secondTime)).map { times =>
+            play.api.Logger.debug("First: " + times(0) + ", Second: " + times(1))
+
+            val choice = if (times(0) > times(1)) secondChoice
+            else firstChoice
+
+            play.api.Logger.debug("Choice: " + choice(0))
+            val json = Json.obj(
+              "status" -> "OK",
+              "redirect" -> Json.obj(
+                "winner" -> choice(0),
+                "results" -> Json.arr(
+                  Json.obj(
+                    "url" -> firstChoice(0),
+                    "name" -> firstChoice(1),
+                    "time" -> times(0)),
+                  Json.obj(
+                    "url" -> secondChoice(0),
+                    "name" -> secondChoice(1),
+                    "time" -> times(1)))))
+            
+            play.api.cache.Cache.set("race.results", json, 60 * 60 * 24)
+            Ok(json)
+          } recover {
+            case e =>
+              play.api.Logger.warn("Issue fetching url: " + e.getMessage)
+              val json = Json.obj(
+                "status" -> "KO",
+                "errors" -> Json.arr(
+                  "Something went wrong!",
+                  e.getMessage))
+              InternalServerError(json)
+          }
+        }
       }
     }
   }
